b64d15142d5648d1f003b98f50c0792fee2a66da
dirkdior <dirkdior@gmail.com>
2021-03-09 02:42:20 +0100

    Use actionlib to send robot a goal

--- src/moveturtlebot.cpp
+++ src/moveturtlebot.cpp
@@ -1,76 +1,54 @@
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
-#include <stdlib.h>
-#include <std_srvs/Empty.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
 
-void stopBot(ros::Publisher publisher) {
-    geometry_msgs::Twist msg;
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-    msg.linear.x  = 0.0;
-    msg.angular.z = 0.0;
+void moveBot(double goalPosX, double goalOrntW) {
 
-    publisher.publish(msg);
-    ros::spinOnce();
+    MoveBaseClient ac("move_base", true);
+
+    while(!ac.waitForServer(ros::Duration(5.0))){
+        ROS_INFO("Waiting for the move_base action server to come up");
+    }
+
+    move_base_msgs::MoveBaseGoal goal;
+
+    goal.target_pose.header.frame_id = "base_link";
+    goal.target_pose.header.stamp = ros::Time::now();
+
+    goal.target_pose.pose.position.x = goalPosX;
+    goal.target_pose.pose.orientation.w = goalOrntW;
+
+    ROS_INFO("Sending goal");
+    ac.sendGoal(goal);
+
+    ac.waitForResult();
+   
+    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+        ROS_INFO("Hooray, the base moved 1 meter forward");
+    else
+        ROS_INFO("The base failed to move forward 1 meter for some reason");
 }
 
-void moveBot(ros::Publisher publisher, double linX, double angZ) {
-    geometry_msgs::Twist msg;
-
-    msg.linear.x  = linX;
-    msg.angular.z = angZ;
-
-    publisher.publish(msg);
-    ros::spinOnce();
-}
-
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     ros::init(argc, argv, "moveturtlebot");
+    
+    moveBot(
+        5.0,
+        2.0
+    );
+    ros::Duration(30).sleep();
+    moveBot(
+        1.0,
+        6.0
+    );
+    ros::Duration(30).sleep();
+    
+    moveBot(
+        3.0,
+        6.0
+    );
 
-    ros::NodeHandle node_handle;
-
-    ros::Publisher publisher = node_handle.advertise<geometry_msgs::Twist>("/cmd_vel", 20, true);
-
-    //call global_localization service with empty
-    // ros::service::waitForService("global_localization");
-    // ros::ServiceClient clearClient = node_handle.serviceClient<std_srvs::Empty>("/global_localization");
-    // std_srvs::Empty empty_srv;
-    // if(clearClient.call(empty_srv)) {
-    //     ROS_ERROR("reset global_localization");
-    // } else {
-    //     ROS_ERROR("Failed to call the global_localization service");
-    // }
-
-    while(publisher.getNumSubscribers()==0)
-    {
-        ROS_ERROR("Waiting for subsribers");
-        // system("rosnode kill /map_server"); //kill map_server node
-        // system("rosnode kill /amcl"); //kill amcl node
-        ros::Duration(3).sleep();
-    }
-
-    ROS_ERROR("Got subsriber");
-
-    // Spin the robot for 30 seconds to converge amcl particles
-    // moveBot(publisher, 0.01, 0.5);
-    // ros::Duration(30).sleep();
-    // stopBot(publisher);
-
-    //reset global_localization again
-    // if(clearClient.call(empty_srv)) {
-    //     ROS_ERROR("reset global_localization");
-    // } else {
-    //     ROS_ERROR("Failed to call the global_localization service");
-    // }
-
-    // Spin the robot for another 60 seconds to converge amcl particles all over again for accuracy
-    // moveBot(publisher, 0.01, 0.5);
-    // ros::Duration(60).sleep();
-    // stopBot(publisher);
-
-    while(ros::ok())
-    {
-        ros::Duration(3).sleep();    
-    }
-
+    return 0;
 }
